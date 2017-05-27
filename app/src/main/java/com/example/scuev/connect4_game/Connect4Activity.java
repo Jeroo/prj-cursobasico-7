18ecdb7fb7cdbe610ecb5da1b50c9644c4a8098f
Jeroo <SALva18??>
2017-05-27 02:16:47 -0400

    Commit branch. Animations

--- app/src/main/java/com/example/scuev/connect4_game/Connect4Activity.java
+++ app/src/main/java/com/example/scuev/connect4_game/Connect4Activity.java
@@ -1,16 +1,21 @@
 package com.example.scuev.connect4_game;
 
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.HashMap;
@@ -27,6 +32,11 @@ public class Connect4Activity extends AppCompatActivity implements View.OnClickL
     private String                   won;
     private LinearLayout             row0,row1,row2,row3,row4,row5;
     private Map<String,LinearLayout> listRows;
+    private ViewGroup                mainLayout;
+    private Button                   buttonCirclePlay;
+
+    private int xDelta;
+    private int yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,6 +58,16 @@ public class Connect4Activity extends AppCompatActivity implements View.OnClickL
         row5 = (LinearLayout)findViewById(R.id.layout_circlesRow6);
 
         generateVector();
+
+        mainLayout = (RelativeLayout) findViewById(R.id.main);
+        buttonCirclePlay = (Button) findViewById(R.id.image);
+
+        buttonCirclePlay.setOnTouchListener(onTouchListener());
+
+        ScrollView sView = (ScrollView)findViewById(R.id.my_scrolview);
+        // Hide the Scollbar
+        sView.setVerticalScrollBarEnabled(false);
+        sView.setHorizontalScrollBarEnabled(false);
 
         //Get evens from imageButtons
         findViewById(R.id._0_0).setOnClickListener(this);
@@ -164,6 +184,7 @@ public class Connect4Activity extends AppCompatActivity implements View.OnClickL
                                 buttonTurn.setBackgroundColor(Color.RED);
                                 objConnect4.fillMatrix(varPosXInt, varPosYInt, 1);
                                 won = "Blue Win!";
+                                buttonCirclePlay.setBackgroundResource(R.drawable.red);
 
                             } else if (objConnect4.getTurn() == 2)
                             {
@@ -174,6 +195,7 @@ public class Connect4Activity extends AppCompatActivity implements View.OnClickL
                                 buttonCircle.setTag("red");
                                 objConnect4.fillMatrix(varPosXInt, varPosYInt, 2);
                                 won = "Red Win!";
+                                buttonCirclePlay.setBackgroundResource(R.drawable.blue);
                             }
                         }
                     }
@@ -244,5 +266,47 @@ public class Connect4Activity extends AppCompatActivity implements View.OnClickL
 
         childCount = 0;
 
+    }
+
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        Toast.makeText(Connect4Activity.this,
+                                "xDelta: "+x+" Ydelta"+y, Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                .getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        };
     }
 }
